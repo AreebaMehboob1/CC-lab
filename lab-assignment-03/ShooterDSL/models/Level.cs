30fdfdb278dfb5c978cadae80850405f7c42162d
AreebaMehboob1 <fa21-bcs-034@cuiatk.edu.pk>
2025-01-16 17:29:50 +0500

    Delete lab-assignment-03 directory

--- lab-assignment-03/ShooterDSL/models/Level.cs
+++
@@ -1,9 +0,0 @@
-namespace GalaxyShooterDSL.Models;
-
-public class Level
-{
-    public int LevelNumber { get; set; }  // Represents the level number, e.g., "1" for Level 1.
-    public string[] Ships { get; set; }  // An array of ship names used in the level (e.g., ["Hero"]).
-    public string[] Waves { get; set; }  // An array of wave names associated with the level (e.g., ["Wave 1", "Wave 2"]).
-    public string[] PowerUps { get; set; } // An array of power-up names available in the level (e.g., ["Shield", "SpeedBoost"]).
-}
